9d38a3dd4727e9033edc3fa0e080ae235bf894ff
4rcherry <bilawalfr@gmail.com>
2018-06-18 14:24:57 +0700

    Create library.php

---
+++ apis/library.php
@@ -0,0 +1,32 @@
+<?php
+
+$CIT = "001";	//Computing Information Technology
+$EEN = "002";	//Electronical Engineering
+$MEN = "003";	//Mechanical Engineering
+$IEN = "004";	//Industrial Engineering
+//$A = "005";	
+//$B = "006";
+//$C = "007";
+$ACC = "008";	//Accounting
+$COM = "009";	//Communication
+//$D = "010";
+//$E = "011";
+$CIS = "012";	//Computing Information System
+$VCD = "013";	//Visual Communication Design
+$MGT = "014";	//Management
+$BAD = "015";	//Business Administration
+$IRE = "016";	//International Relation
+$LAW = "017";	//Law
+$ENV = "018";	//Environmental Engineering
+$PSD = "019";	//Pendidikan Guru Sekolah Dasar
+
+//$batch = "2004";
+$batch14 = "2014";
+$batch15 = "2015";
+$batch16 = "2016";
+$batch17 = "2017";
+
+$morning = "00";
+$evening = "05";
+
+$class 	= $IRE.$batch17.$morning;
